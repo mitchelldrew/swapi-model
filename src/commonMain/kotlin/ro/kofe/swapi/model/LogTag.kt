0c69e063bf434dea696ed24a56e027e5667398d8
mdrew <mdrew@mdrews-MacBook-Pro.local>
2022-02-22 16:02:16 -0800

    master - mdrew: added logtags

---
+++ src/commonMain/kotlin/ro/kofe/swapi/model/LogTag.kt
@@ -0,0 +1,10 @@
+package ro.kofe.swapi.model
+
+object LogTag {
+    val FILM_INTERACTOR = "FilmInteractor"
+    val FILM_PRESENTER = "FilmPresenter"
+    val PEOPLE_INTERACTOR = "PeopleInteractor"
+    val PEOPLE_PRESENTER = "PeoplePresenter"
+    val PLANET_INTERACTOR = "PlanetInteractor"
+    val PLANET_PRESENTER = "PlanetPresenter"
+}
